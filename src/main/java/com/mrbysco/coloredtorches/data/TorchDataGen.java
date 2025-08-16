c57c1f42cb972877d1cb78c25f7515f6518c3571
Mrbysco <Mrbysco@users.noreply.github.com>
2025-08-16 21:57:05 +0200

    Port to 1.21.8

--- src/main/java/com/mrbysco/coloredtorches/data/TorchDataGen.java
+++ src/main/java/com/mrbysco/coloredtorches/data/TorchDataGen.java
@@ -5,18 +5,16 @@ import com.mrbysco.coloredtorches.data.assets.TorchModelProvider;
 import com.mrbysco.coloredtorches.data.data.TorchLootProvider;
 import com.mrbysco.coloredtorches.data.data.TorchRecipeProvider;
 import com.mrbysco.coloredtorches.data.data.tags.TorchBlockTagsProvider;
-import com.mrbysco.coloredtorches.data.data.tags.TorchItemTagsProvider;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.common.data.BlockTagsProvider;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 
 import java.util.concurrent.CompletableFuture;
 
-@EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber
 public class TorchDataGen {
 	@SubscribeEvent
 	public static void gatherData(GatherDataEvent.Client event) {
@@ -26,9 +24,7 @@ public class TorchDataGen {
 
 		generator.addProvider(true, new TorchLootProvider(packOutput, lookupProvider));
 		generator.addProvider(true, new TorchRecipeProvider.Runner(packOutput, lookupProvider));
-		BlockTagsProvider provider;
-		generator.addProvider(true, provider = new TorchBlockTagsProvider(packOutput, lookupProvider));
-		generator.addProvider(true, new TorchItemTagsProvider(packOutput, lookupProvider, provider));
+		generator.addProvider(true, new TorchBlockTagsProvider(packOutput, lookupProvider));
 
 		generator.addProvider(true, new TorchLanguageProvider(packOutput));
 		generator.addProvider(true, new TorchModelProvider(packOutput));
